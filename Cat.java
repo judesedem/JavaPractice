3ba3789c72a02a60fc087a029129da5621e0e543
judesedem <judefiadzawoo@gmail.com>
2026-04-11 09:23:17 +0000

    Practicing Inheritance

---
+++ Cat.java
@@ -0,0 +1,11 @@
+public class Cat extends Animal {
+    int lives=9;
+
+    void speak(){
+        System.out.println("The cat goes meow!");
+    }
+
+
+
+    
+}
---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args){
+    Dog dog=new Dog();
+    Cat cat=new Cat();
+
+    dog.eat();
+    cat.eat();
+    dog.speak();
+    cat.speak();
+    }
+
+  
+}
